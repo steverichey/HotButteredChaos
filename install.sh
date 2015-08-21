9185e46d6f03b58e0f0470bf7177c396957e4ccb
Steve Richey <srichey@floatlearning.com>
2015-08-21 11:40:36 -0500

    exit install script on error

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# exit script on error
+set -e
+
 # see https://jonathan.porta.codes/2015/04/17/automatically-build-your-unity3d-project-in-the-cloud-using-travisci-for-free/
 
 echo 'Downloading from http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg: '
