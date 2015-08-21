8f260c95e296054dc1f2a54d40223d1eb928b3f7
Steve Richey <srichey@floatlearning.com>
2015-08-21 08:07:25 -0500

    experimental travis integration

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# see https://jonathan.porta.codes/2015/04/17/automatically-build-your-unity3d-project-in-the-cloud-using-travisci-for-free/
+
+echo 'Downloading from http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg: '
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg
+
+echo 'Installing Unity.pkg'
+sudo installer -dumplog -package Unity.pkg -target /
