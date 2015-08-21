6e069b477c14f2a34e00d2c721d0a1b4574c5a17
Steve Richey <srichey@floatlearning.com>
2015-08-21 11:58:37 -0500

    don't try to compress iOS/Android builds if they failed

--- compress.sh
+++ compress.sh
@@ -4,5 +4,13 @@ zip Archives/linux.zip Builds/LinuxUniversal/*
 zip Archives/osx.zip Builds/OSXUniversal/*
 zip Archives/win32.zip Builds/Win32/*
 zip Archives/win64.zip Builds/Win64/*
-zip Archives/android.zip Builds/Android/*
-zip Archives/ios.zip Builds/iOS/*
+
+if [ -d "/Builds/Android" ]
+then
+  zip Archives/android.zip Builds/Android/*
+fi
+
+if [ -d "/Builds/iOS" ]
+then
+  zip Archives/ios.zip Builds/iOS/*
+fi
